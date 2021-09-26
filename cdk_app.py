aaa5822a50c2accde82837288812572483fe986f
Jianhua Wu <jianhua.wu@growsuper.com>
2021-09-26 23:46:42 +1000

    seperated the logic of vpc creation and subnets addition

--- cdk_app.py
+++ cdk_app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import os
+import time
 from constructs import Construct
 from aws_cdk import App, Stack, Environment
 from aws_cdk import aws_s3 as s3
@@ -20,6 +21,7 @@ vpc_stack = VPCStack(app_vpc, "DREAMCHASER-VPC-STACK-MAIN",
                         region=aws_region
                      )
             )
+
 # assume you don't have AWS Organization set up, and there is one AWS account only,
 if app_vpc.node.try_get_context("EASY_VPC"):
         vpc_stack.easy_vpc()
@@ -27,20 +29,45 @@ else:
         # the purpose of the lines of code below is to show how DreamChaser construct can be used flexibibly
         # you can make changes per your project requirements
         ou_id = app_vpc.node.try_get_context("OU_ID") or ssm_client.get_para(name='DC_ONE_OU_ID')
-        vpc_stack.vpc(vpc_cidr=app_vpc.node.try_get_context('DC_VPC_CIDR'), vpc_ha=app_vpc.node.try_get_context('DC_VPC_HA'))
-        vpc_stack.apply_endpoint(vpc_id=vpc_stack.vpc.vpc_id, rt_ids=vpc_stack.route_tables)
-        vpc_stack.create_tgw()
-        vpc_stack.share_tgw(ou_id=ou_id, tgw_id=app_vpc.node.try_get_context("TGW_ID") or vpc_stack.tgw.ref)
-        vpc_stack.create_tgw_attach(
+        vpc_stack.add_vpc(vpc_cidr=app_vpc.node.try_get_context('DC_VPC_CIDR'), enable_internet=True, enable_nat=True, vpc_ha=app_vpc.node.try_get_context('DC_VPC_HA'), vpc_endpoint=True)
+        res_pub_isubnets = vpc_stack.add_public_subnets(subnet_oct3=16, mask=24, scope_id='DCPublicSubnets')
+        res_pri_isubnets = vpc_stack.add_private_subnets(subnet_oct3=128, mask=24, scope_id='DCPrivateSubnets')
+        res_iso_isubnets = vpc_stack.add_isolated_subnets(subnet_oct3=192, mask=24, scope_id='DCIsolatedSubnets')
+        vpc_stack.create_tgw(scope_id="TGW")
+        time.sleep(5)
+        vpc_stack.share_tgw(ou_id=ou_id)
+        res_tgw_isubnets = vpc_stack.create_tgw_attach(
                 vpc_id=app_vpc.node.try_get_context("VPC_ID") or vpc_stack.vpc.vpc_id,
                 tgw_id=app_vpc.node.try_get_context("TGW_ID") or vpc_stack.tgw.ref,
-                tgw_subnets=app_vpc.node.try_get_context("TGW_SUBNETS") or vpc_stack.tgw_subnets,
+                subnet_oct3=240,
+                mask=24,
+                scope_id="TGWAttach"
         )
-        vpc_stack.create_tgw_route(
+        vpc_stack.tgw_route_table( 
                 tgw_id=app_vpc.node.try_get_context("TGW_ID") or vpc_stack.tgw.ref,
                 asso_tgw_attach_id=app_vpc.node.try_get_context("ASSO_TGW_ATTACH_ID") or vpc_stack.tgw_attach.ref,
                 dest_tgw_attach_id=app_vpc.node.try_get_context("DEST_TGW_ATTACH_ID") or vpc_stack.tgw_attach.ref,
                 dest_cidr=app_vpc.node.try_get_context("DC_DEST_CIDR")      # specify your own destination CIDR here.
+        )
+        vpc_stack.add_tgw_route(route_tables=[isubnet.route_table.route_table_id for isubnet in res_pri_isubnets], 
+                tgw_id=vpc_stack.tgw.ref,
+                tgw_attach=vpc_stack.tgw_attach,
+                dest_cidr=app_vpc.node.try_get_context('DC_DEST_CIDR'),
+                scope_id='pritgwroute'
+        )
+        vpc_stack.add_tgw_route(route_tables=[isubnet.route_table.route_table_id for isubnet in res_iso_isubnets],
+                tgw_id=vpc_stack.tgw.ref,
+                tgw_attach=vpc_stack.tgw_attach,
+                dest_cidr=app_vpc.node.try_get_context('DC_DEST_CIDR'),
+                scope_id='isotgwroute'
+        )
+
+        vpc_stack.add_tgw_route(
+            route_tables=[isubnet.route_table.route_table_id for isubnet in res_tgw_isubnets], 
+            tgw_id=vpc_stack.tgw.ref,
+            tgw_attach=vpc_stack.tgw_attach,
+            dest_cidr=vpc_stack.node.try_get_context('DC_DEST_CIDR'),
+            scope_id='tgwroute'
         )
 
 app_vpc.synth()
--- vpc/vpc_core.py
+++ vpc/vpc_core.py
@@ -1,10 +1,13 @@
 import sys
+import uuid
+import time
+import hashlib
 from constructs import Construct
 from aws_cdk import (
     aws_ec2 as ec2,
     aws_iam as iam,
     aws_ram as ram,
-    Stack, Tags
+    Stack, Tags, CfnJson, IResolvable
 )
 
 class VPCStack(Stack):
@@ -12,104 +15,108 @@ class VPCStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-    def vpc(self, vpc_cidr: str, vpc_ha: bool):    
-        vpc_cidr = vpc_cidr
-        oct_1 = vpc_cidr.split('.')[0]
-        oct_2 = vpc_cidr.split('.')[1]
-        total_az = len(self.availability_zones)
-        sum = total_az
-        exp = 0
-        while sum > 0:
-            sum = sum // 2
-            exp = exp + 1
-
-        pub_subnet_oct3 = 2 ** exp * 2         # the IP address range of the public subnets for workload is from x.x.2 ** exp.0
-        pri_subnet_oct3 = 2 ** exp * 4      # the IP address range of the private subnets for workload is from x.x.2 ** exp * 4.0
-        iso_subnet_oct3 = 160               # the IP address range of isolated subnets is from x.x.192.0 to x.x.255.255
-        db_subnet_oct3 = 192
-        tgw_subnet_oct3 = 240
-        subnet_index = 0
-        nat_gateway_ids = []
-        self.route_tables = []
-        self.tgw_subnets = []
-        self.db_subnets = []
-
-        self.vpc = ec2.Vpc(self, 'VPC',
-            cidr=vpc_cidr,
-            max_azs=total_az,
-            enable_dns_hostnames=True,
-            enable_dns_support=True,
+    def add_vpc(self, vpc_cidr: str, enable_internet: bool, enable_nat: bool, vpc_ha: bool, vpc_endpoint: bool=False):
+        self.vpc_cidr = vpc_cidr
+        self.oct_1 = self.vpc_cidr.split('.')[0]
+        self.oct_2 = self.vpc_cidr.split('.')[1]
+        self.enable_internet = enable_internet
+        self.enable_nat = enable_nat
+        self.vpc_ha = vpc_ha
+        self.vpc_endpoint = vpc_endpoint
+        self.nat_gateway_ids = []
+        if self.enable_internet or self.enable_nat:
             subnet_configuration=[
                 ec2.SubnetConfiguration(
                     name="Public",
                     subnet_type=ec2.SubnetType.PUBLIC,
                     cidr_mask=24
                 )
-            ],
-            nat_gateways=None
-        )        
+            ]
+        else:
+            subnet_configuration=[]
 
-        # create Public, Private, Database, Isolated and tgw subnets in each availability zones respectivly
-        for az in self.vpc.availability_zones:
-            public_subnet = ec2.PublicSubnet(self, f"Public Subnet Zone {az}",
-                cidr_block='.'.join((oct_1, oct_2, str(pub_subnet_oct3 + subnet_index), "0")) + "/24",
+        self.vpc = ec2.Vpc(self, 'VPC',
+            cidr=self.vpc_cidr,
+            max_azs=len(self.availability_zones),
+            enable_dns_hostnames=True,
+            enable_dns_support=True,
+            subnet_configuration=subnet_configuration,
+            nat_gateways=None
+        )
+
+        # add AWS Nat Gateways if Nat Gateway is demanded, and there is internet connectivity for the VPC
+        if self.enable_nat:
+            if self.vpc_ha:
+                for zone_index in range(len(self.availability_zones)):
+                    eip = ec2.CfnEIP(self, f"NatGateway EIP {self.availability_zones[zone_index]}")
+                    nat_gateway = ec2.CfnNatGateway(self, f"NatGateway {self.availability_zones[zone_index]}",
+                        subnet_id=self.vpc.public_subnets[zone_index].subnet_id,
+                        allocation_id=eip.attr_allocation_id,
+                        connectivity_type="public"
+                    )
+                    self.nat_gateway_ids.append(nat_gateway.ref)
+            else:
+                eip = ec2.CfnEIP(self, f"NatGateway EIP {self.availability_zones[0]}")
+                nat_gateway = ec2.CfnNatGateway(self, f"NatGateway {self.availability_zones[0]}",
+                    subnet_id=self.vpc.public_subnets[0].subnet_id,
+                    allocation_id=eip.attr_allocation_id,
+                    connectivity_type="public"
+                )
+                self.nat_gateway_ids.append(nat_gateway.ref)
+
+    def add_public_subnets(self, subnet_oct3: int, mask: int, scope_id: str):
+        if not self.enable_internet:
+            raise ValueError('There has to be an Internet Gateway for adding public subnets')
+
+        isubnets = []
+        for index in range(len(self.availability_zones)):
+            public_subnet = ec2.PublicSubnet(self, f"{scope_id} {self.availability_zones[index]}",
+                cidr_block='.'.join((self.oct_1, self.oct_2, str(subnet_oct3 + index), "0")) + "/24",
                 vpc_id=self.vpc.vpc_id,
-                availability_zone=az
+                availability_zone=self.availability_zones[index]
             )
             public_subnet.add_default_internet_route(self.vpc.internet_gateway_id, self.vpc.internet_connectivity_established)
-            self.route_tables.append(public_subnet.route_table.route_table_id)
+            isubnets.append(public_subnet)
 
-            if vpc_ha:
-                eip = ec2.CfnEIP(self, f"NatGateway EIP {az}")
-                nat_gateway = ec2.CfnNatGateway(self, f"NatGateway {az}",
-                    subnet_id=self.vpc.public_subnets[subnet_index].subnet_id,
-                    allocation_id=eip.attr_allocation_id,
-                    connectivity_type="public"
-                )
-                nat_gateway_ids.append(nat_gateway.ref)
-            elif (len(nat_gateway_ids) < 1):
-                eip = ec2.CfnEIP(self, f"NatGateway EIP {az}")
-                nat_gateway = ec2.CfnNatGateway(self, f"NatGateway {az}",
-                    subnet_id=self.vpc.public_subnets[subnet_index].subnet_id,
-                    allocation_id=eip.attr_allocation_id,
-                    connectivity_type="public"
-                )
-                nat_gateway_ids.append(nat_gateway.ref)
+        if self.vpc_endpoint:
+            self.apply_endpoint(rt_ids=[isubnet.route_table.route_table_id for isubnet in isubnets], scope_id=scope_id)
 
+        return isubnets
 
-            private_subnet = ec2.PrivateSubnet(self, f"Private Subnet Zone {az}",
-                cidr_block='.'.join((oct_1, oct_2, str(pri_subnet_oct3 + subnet_index), "0")) + "/24",
+    def add_private_subnets(self, subnet_oct3: int, mask: int, scope_id: str):
+        isubnets = []
+        for index in range(len(self.availability_zones)):
+            private_subnet = ec2.PrivateSubnet(self, f"{scope_id} {self.availability_zones[index]}",
+                cidr_block='.'.join((self.oct_1, self.oct_2, str(subnet_oct3 + index), "0")) + "/24",
                 vpc_id=self.vpc.vpc_id,
-                availability_zone=az
+                availability_zone=self.availability_zones[index]
             )
-            private_subnet.add_default_nat_route(nat_gateway_ids[subnet_index] if vpc_ha else nat_gateway_ids[0])
-            self.route_tables.append(private_subnet.route_table.route_table_id)
+            isubnets.append(private_subnet)
 
-            db_subnet = ec2.Subnet(self, f"Database Subnet Zone {az}",
-                cidr_block='.'.join((oct_1, oct_2, str(db_subnet_oct3 + subnet_index), "0")) + "/24",
+        if self.enable_nat:
+            self.add_nat_route(isubnets=isubnets)
+        
+        if self.vpc_endpoint:
+            self.apply_endpoint(rt_ids=[isubnet.route_table.route_table_id for isubnet in isubnets], scope_id=scope_id)
+
+        return isubnets
+
+    def add_isolated_subnets(self, subnet_oct3: int, mask: int, scope_id: str):
+        isubnets = []
+        for index in range(len(self.availability_zones)):
+            subnet = ec2.Subnet(self, f"{scope_id} {self.availability_zones[index]}",
+                cidr_block='.'.join((self.oct_1, self.oct_2, str(subnet_oct3 + index), "0")) + "/24",
                 vpc_id=self.vpc.vpc_id,
-                availability_zone=az
+                availability_zone=self.availability_zones[index]
             )
-            self.db_subnets.append(db_subnet.subnet_id)
+            isubnets.append(subnet)
+        
+        return isubnets
 
-            isolated_subnet = ec2.Subnet(self, f"Isolated Subnet Zone {az}",
-                cidr_block='.'.join((oct_1, oct_2, str(iso_subnet_oct3 + subnet_index), "0")) + "/24",
-                vpc_id=self.vpc.vpc_id,
-                availability_zone=az
-            )
-
-            tgw_subnet = ec2.Subnet(self, f"TGW Subnet Zone {az}",
-                cidr_block='.'.join((oct_1, oct_2, str(tgw_subnet_oct3 + subnet_index), "0")) + "/24",
-                vpc_id=self.vpc.vpc_id,
-                availability_zone=az
-            )
-            self.tgw_subnets.append(tgw_subnet.subnet_id)
-            subnet_index = subnet_index + 1
-
-    def apply_endpoint(self, vpc_id: str, rt_ids: list) -> None:
-        ec2.CfnVPCEndpoint(self, "VPCEnpoint",
+    def apply_endpoint(self, rt_ids: list, scope_id: str) -> None:
+        ec2.CfnVPCEndpoint(self, f"{scope_id} VPCEndpoint",
             service_name="com.amazonaws." + self.region + ".s3",
-            vpc_id=vpc_id,
+            vpc_id=self.vpc.vpc_id,
             route_table_ids=rt_ids,
             vpc_endpoint_type="Gateway",
             policy_document = {
@@ -136,9 +143,17 @@ class VPCStack(Stack):
                 }
         )
 
-    def create_tgw(self):
-        self.tgw = ec2.CfnTransitGateway(self, "TGW",
-            amazon_side_asn=65000,
+    def add_nat_route(self, isubnets: list):
+        for i in range(len(self.availability_zones)):
+            isubnets[i].add_default_nat_route(self.nat_gateway_ids[i] if self.vpc_ha else self.nat_gateway_ids[0])
+
+    def create_tgw(self, bgp_asn: int=65000, scope_id: str=None):
+        # unique bgp_asn number is needed if you have multiple transit gateway in your organization.
+        # and the value should not conflict with the other ASN value (if there is any) that you use
+        # in your organization
+        self.tgw_scope_id = scope_id
+        self.tgw = ec2.CfnTransitGateway(self, scope_id,
+            amazon_side_asn=bgp_asn,
             auto_accept_shared_attachments='enable',
             default_route_table_association='disable',
             default_route_table_propagation='disable',
@@ -147,46 +162,88 @@ class VPCStack(Stack):
         )
 
     # share your transit gateway in your aws organization
-    def share_tgw(self, ou_id: str, tgw_id: str) -> None:
-        ram.CfnResourceShare(self, 'RAMTgw',
-            name='dc_one_ou_id',
+    def share_tgw(self, ou_id: str) -> None:
+        ram.CfnResourceShare(self, 'RAM{self.tgw_scope_id}',
+            name='dc_one_tgw_id_' + self.tgw_scope_id.lower(),
             principals=[ou_id],
-            resource_arns=[f"arn:aws:ec2:{self.region}:{self.account}:transit-gateway/{tgw_id}"]
+            resource_arns=[f"arn:aws:ec2:{self.region}:{self.account}:transit-gateway/{self.tgw.ref}"]
         )
 
-    def create_tgw_attach(self, vpc_id: str, tgw_id: str, tgw_subnets: str):
-        self.tgw_attach = ec2.CfnTransitGatewayAttachment(self, "TGWAttach",
-            subnet_ids=tgw_subnets,
+    def create_tgw_attach(self, vpc_id: str, tgw_id: str, subnet_oct3: int, mask: int, scope_id: str=None):
+        """
+        Create Transit Gateway Attachment
+
+        Per AWS best practice, each TGW should have its own set of VPC subnets in a VPC. Hence, The 
+        creation of VPC subnets is definied along with the definition of adding TGW attachment.
+
+        TGW ID has to be passed in as a function argument(tgw_id) as there is chance that tgw to have
+        a tgw attachment attached to is shared via AWS Resource Access Manager.
+
+        VPC ID should be passed in as a function argument(vpc_id) since there is circumstance that
+        TGW attachment is added into an existing VPC.
+        """
+
+        # since there is circumstance that TGW attachment is added into an existing VPC, it merits to
+        # create a Class which is dedicated to handling TGW related resource definition.
+        if self.enable_nat:
+            res_isubnets = self.add_private_subnets(subnet_oct3=subnet_oct3, mask=mask, scope_id=scope_id)
+        else:
+            res_isubnets = self.add_isolated_subnets(subnet_oct3=subnet_oct3, mask=mask, scope_id=scope_id)
+
+        self.tgw_attach = ec2.CfnTransitGatewayAttachment(self, scope_id,
+            subnet_ids=[isubnet.subnet_id for isubnet in res_isubnets],
             transit_gateway_id=tgw_id,
             vpc_id=vpc_id
         )
+        return res_isubnets
 
-    def create_tgw_route(self, tgw_id: str, asso_tgw_attach_id: str, dest_tgw_attach_id: str, dest_cidr: str):
-        self.tgw_rt = ec2.CfnTransitGatewayRouteTable(self, "TGWRt",
+    def tgw_route_table(self, tgw_id: str, asso_tgw_attach_id: str, dest_tgw_attach_id: str, dest_cidr: str):
+        """
+        Implement Flat Transit Gateway route domains
+
+        """
+
+        # to implement isolated Transit Gateway route domains, coding logic needs to be improved.
+        self.tgw_rt = ec2.CfnTransitGatewayRouteTable(self, "{self.tgw_scope_id}Rt",
             transit_gateway_id=tgw_id
         )
 
-        self.tgw_rt_asso = ec2.CfnTransitGatewayRouteTableAssociation(self, "TGWRtAssociation",
+        self.tgw_rt_asso = ec2.CfnTransitGatewayRouteTableAssociation(self, "{self.tgw_scope_id}RtAssociation",
             transit_gateway_attachment_id=asso_tgw_attach_id,
             transit_gateway_route_table_id=self.tgw_rt.ref
         )
 
-        self.tgw_route = ec2.CfnTransitGatewayRoute(self, "TGWRoute",
+        self.tgw_route = ec2.CfnTransitGatewayRoute(self, "{self.tgw_scope_id}TGWRoute",
             transit_gateway_route_table_id=self.tgw_rt.ref,
             destination_cidr_block=dest_cidr,
             transit_gateway_attachment_id=dest_tgw_attach_id,
 
         )
 
+    def add_tgw_route(self, route_tables: list, tgw_id: str, tgw_attach: str, dest_cidr: str, scope_id: str=None):
+        for i in range(len(route_tables)):
+            res_tgw_route=ec2.CfnRoute(self, f"{scope_id}{i}",
+                            route_table_id=route_tables[i],
+                            transit_gateway_id=tgw_id,
+                            destination_cidr_block=dest_cidr
+            )
+            res_tgw_route.add_depends_on(tgw_attach)
+
     # create vpc in dreamchaser way easily
     def easy_vpc(self):
-        self.vpc(vpc_cidr=self.node.try_get_context('DC_VPC_CIDR'), vpc_ha=self.node.try_get_context('DC_VPC_HA'))
-        self.apply_endpoint(vpc_id=self.vpc.vpc_id, rt_ids=self.route_tables)
-        self.create_tgw()
-        self.create_tgw_attach(vpc_id=self.vpc.vpc_id, tgw_id=self.tgw.ref, tgw_subnets=self.tgw_subnets)
+        self.add_vpc(vpc_cidr=self.node.try_get_context('DC_VPC_CIDR'), enable_internet=False, enable_nat=True, vpc_ha=self.node.try_get_context('DC_VPC_HA'), vpc_endpoint=True)
+        #res_pub_isubnets = self.add_public_subnets(subnet_oct3=16, mask=24, scope_id='DCPublicSubnets')
+        res_pri_isubnets = self.add_private_subnets(subnet_oct3=128, mask=24, scope_id='DCPrivateSubnets')
+        res_iso_isubnets = self.add_isolated_subnets(subnet_oct3=192, mask=24, scope_id='DCIsolatedSubnets')
+        self.create_tgw(scope_id="TGW")
+        res_tgw_isubnets = self.create_tgw_attach(vpc_id=self.vpc.vpc_id, tgw_id=self.tgw.ref, subnet_oct3=240, mask=24, scope_id="TGWAttach")
+        self.add_tgw_route(
+            route_tables=[isubnet.route_table.route_table_id for isubnet in res_pri_isubnets], 
+            tgw_id=self.tgw.ref, tgw_attach=self.tgw_attach, dest_cidr=self.node.try_get_context('DC_DEST_CIDR'), scope_id='tgwpriroute')
+        self.add_tgw_route(route_tables=[isubnet.route_table.route_table_id for isubnet in res_iso_isubnets], 
+            tgw_id=self.tgw.ref, tgw_attach=self.tgw_attach, dest_cidr=self.node.try_get_context('DC_DEST_CIDR'), scope_id='tgwisoroute')
+        self.add_tgw_route(
+            route_tables=[isubnet.route_table.route_table_id for isubnet in res_tgw_isubnets], 
+            tgw_id=self.tgw.ref, tgw_attach=self.tgw_attach, dest_cidr=self.node.try_get_context('DC_DEST_CIDR'), scope_id='tgwroute')
         
     # Tags.of(self.vpc).add("DREAMCHASER", "DREAMCHASERVPC")
-
-    # self.vpc.from_lookup(self, 'MYVPC',
-    #     tags = { "DREAMCHASER": "DREAMCHASERVPC" }
-    # )
